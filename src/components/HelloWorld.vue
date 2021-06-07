2bf89d5bd96cc72b4c073aa7be13c3582f268a8c
Mayur <mayurpawar.co.in@email.com>
2021-06-07 17:34:01 +0530

    complex form created

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,13 +7,37 @@
     <label>Password</label>
     <input type="Password" placeholder="Enter Password" v-model="dataCollector.password">
     <br><br>
+    <label>Country</label>
+    <select v-model="dataCollector.country">
+        <option>India</option>
+        <option>Japan</option>
+        <option>USA</option>
+    </select>
+    <br><br>
+    <h3>Technology</h3><br>
+    <label>Python</label>
+    <input type="checkbox" value="Python" v-model="dataCollector.tech">
+    <br>
+    <label>Vue Js</label>
+    <input type="checkbox" value="Vue Js" v-model="dataCollector.tech">
+    <br>
+    <label>C++</label>
+    <input type="checkbox" value="C++" v-model="dataCollector.tech">
+    <br><br>
+    <h3>Gender</h3>
+    <label for=male>Male</label>
+    <input type="radio" id="male" name="gender" value="Male" v-model="dataCollector.gender">
+    <br>
+    <label for="female">Female</label>
+    <input type="radio" value="Female" id="female" name="gender" v-model="dataCollector.gender">
+    <br><br>
     <button v-on:click="login" type="button">Login</button>
 </form>
 <div>
-  <!-- {{dataCollector.email}} -->
-  <br><br>
-  <h3>Form Data</h3>
-  {{dataCollector}}
+    <!-- {{dataCollector.email}} -->
+    <br><br>
+    <h3>Form Data</h3>
+    {{dataCollector}}
 </div>
 </template>
 
@@ -24,7 +48,10 @@ export default {
         return {
             dataCollector: {
                 email: "",
-                password: ""
+                password: "",
+                country: "",
+                tech: [],
+                gender: "",
             }
         }
     },
